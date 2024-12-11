40e45a7771b8a78dba1012eecf2d1e93839be38e
SmartProfits <whenneedgame02@gmail.com>
2024-12-11 14:58:27 +0800

    Update 11.js

--- 11.js
+++ 11.js
@@ -1,5 +1,4 @@
-        <script>
-        // 分类商品
+
 			        const categories = {
     BanHeang: [
         { id: 101, name: "BH Tambun Original" },
@@ -444,4 +443,3 @@ window.onload = function() {
         }
     }, { once: true }); // 使用 once 选项确保事件监听器只执行一次
 }
-    </script>
